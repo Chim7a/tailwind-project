bb76403895821add37e4d84fcd93c96c4a04123a
chim7a <chimalawrence11@gmail.com>
2024-09-18 13:43:47 +0100

    Hero Section update

---
+++ script.js
@@ -0,0 +1,18 @@
+// var myAge = 19;
+// var origin = "Imo state";
+// var ResizedsIn = "Abuja";
+// var myPet = "a dog";
+// var MyDogsName = "Lisa";
+// var GOAT = "MESSI";
+// var SubGoat = "ronaldo";
+// var favTeam = "Man United";
+// var WorstTeam = "Arsenal";
+
+// console.log(myAge);
+// console.log(origin);
+// console.log(ResizedsIn);
+// console.log(MyDogsName);
+// console.log(GOAT);
+// console.log(SubGoat);
+// console.log(favTeam);
+// console.log(WorstTeam);
